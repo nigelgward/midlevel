0e3fcf24dbc5848fcd15344f38190acbd07db153
Nigel G. Ward <nigelward@acm.org>
2015-12-17 16:46:25 +0900

    documentation fixes

--- src4/bhatd.m
+++
@@ -1,4 +0,0 @@
-function coef = bhatD(distribution1, distribution2)
-% compute the Bhattacharyya distance between two distributions
-% Nigel Ward, UTEP, February 2015
-   coef = -log(sum(sqrt(distribution1 .* distribution2)));
