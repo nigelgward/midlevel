0d33986b9ab419ba39db3d929834487d9321db8a
nigelgward <nigelward@acm.org>
2017-01-05 09:44:58 -0700

    sf

--- src/findClusterMeans.m
+++ src/findClusterMeans.m
@@ -34,3 +34,20 @@ end
 % [silence,speech] = findClusterMeans(e)
 % silence does not end up at the noice floor, but somewhat above it,
 % which is probably fine.
+
+
+function [subsetAverage] = averageOfNearValues(values, near_mean, far_mean)
+% returns the averages of all points which are closer to the near mean
+% than to the far mean
+
+% to save time, approximate by taking a sample of 1000 values 
+nsamples = 1000;
+if length(values) < 1000
+  samples = values;
+else
+  samples = values(1:round(end/nsamples):end);
+end
+closerSamples = samples(abs(samples - near_mean) < abs(samples - far_mean));
+subsetAverage = mean(closerSamples);
+
+end
--- src/windowize.m
+++ src/windowize.m
@@ -1,14 +1,16 @@
 function windowValues = windowize(frameFeatures, msPerWindow)
 
 % inputs:
-%   frameFeatures: features over every 10 millisecond frame, i.e., centered at 5ms, 15ms etc. 
-%   msPerWindow: duration of window to compute windowed values  over
+%   frameFeatures: features over every 10 millisecond frame,
+%     centered at 5ms, 15ms etc. 
+%     A row vector.
+%   msPerWindow: duration of window over which to compute windowed values
 % output:
 %   summed values over windows of the designated size, 
 %     centered at 10ms, 20ms, etc.
 %     (the centering is off, at 15ms, etc, if msPerWindow is 30ms, 50ms etc)
 %      but we're not doing syllable-level prosody, so it doesn't matter.
-%  Values returned are zero if either end of the window would go outside  
+%   values are zero if either end of the window would go outside  
 %     what we have data for. 
 
 % Nigel Ward, UTEP, Feb 2015
@@ -17,8 +19,8 @@ integralImage = [0 cumsum(frameFeatures)];
 framesPerWindow = msPerWindow / 10;
 windowSum = integralImage(1+framesPerWindow:end) - integralImage(1:end-framesPerWindow);
 
-% align so first value is for window centered at 10 ms (or 15ms if, an odd number of frames)
-% using zeros for padding is really not right
+% align so first value is for window centered at 10 ms 
+%  (or 15ms if, an odd number of frames)
 headFramesToPad = floor(framesPerWindow / 2) - 1  ;
 tailFramesToPad = ceil(framesPerWindow / 2) - 1  ;
 windowValues = horzcat(zeros(1,headFramesToPad), ...
