2c7e7f52d77a3e495c37ac13b7c91407cd35873e
nigelgward <nigelward@acm.orgg>
2023-01-25 11:44:36 -0700

    tilt fixed

--- src/allTiltFeatures.m
+++ src/allTiltFeatures.m
@@ -3,41 +3,40 @@
 function [st, tiltRange, tf, tm, tn] = allTiltFeatures( ...
       perFrameTilts, isSpeakingVec, msPerWindow)
 
+  frPerWindow = msPerWindow / 10;
+  globalMeanOfValids = mean(perFrameTilts(isSpeakingVec==true));
+  st = meansOverNonzeros(perFrameTilts, isSpeakingVec, frPerWindow,globalMeanOfValids);
+  
+
   prunedTilts = perFrameTilts;
   prunedTilts(isSpeakingVec == 0) = 0;
-  
-  frPerWindow = msPerWindow / 10;
-  st = meansOverNonzeros(prunedTilts, isSpeakingVec, frPerWindow)
-  
-  %% prepare to get filterband counts per window
   nonzeroTilts = prunedTilts(prunedTilts~=0)
   [bincounts, binedges] = histcounts(nonzeroTilts, 100);
   cumPerc = cumsum(bincounts * 1.0 / sum(bincounts));
-  veryNegThreshold       = ftfp(bincounts, binedges, cumPerc, .20)
-  nearlyFlatThreshold    = ftfp(bincounts, binedges, cumPerc, .50)
-  ninetyEighthPercentile = ftfp(bincounts, binedges, cumPerc, .98)
+  veryNegThreshold       = ftfp(bincounts, binedges, cumPerc, .20);
+  nearlyFlatThreshold    = ftfp(bincounts, binedges, cumPerc, .50);
+  ninetyEighthPercentile = ftfp(bincounts, binedges, cumPerc, .98);
   
+  %% the first two are boolean; but veryNegFrames is scalar
   nearlyFlatFrames = prunedTilts < 0  & ...  
   		     prunedTilts <= ninetyEighthPercentile & ...
-		     prunedTilts > nearlyFlatThreshold
+		     prunedTilts > nearlyFlatThreshold;
   middlingNegFrames = ...     
   		  prunedTilts <= nearlyFlatThreshold & ...
-		  prunedTilts > veryNegThreshold
-  %% only this one is non-boolean
+		  prunedTilts > veryNegThreshold;
   veryNegFrames = prunedTilts;
   veryNegFrames(veryNegFrames > veryNegThreshold) = 0
 
-  tf = meansOverNonzeros(nearlyFlatFrames,  isSpeakingVec, frPerWindow);
-  tm = meansOverNonzeros(middlingNegFrames, isSpeakingVec, frPerWindow);
-  tn = .2 - meansOverNonzeros(veryNegFrames,isSpeakingVec, frPerWindow);
+  tf = meansOverNonzeros(nearlyFlatFrames,  isSpeakingVec, frPerWindow, 0);
+  tm = meansOverNonzeros(middlingNegFrames, isSpeakingVec, frPerWindow, 0);
+  tn = - meansOverNonzeros(veryNegFrames,isSpeakingVec, frPerWindow, 0);
 
-  for i = 1:length(perFrameTilts)
-     tiltRange(i) = max(prunedTilts(i:i+frPerWindow)) - ...
-		    min(prunedTilts(i:i+frPerWindow));
+  for firstFrame = 1:length(perFrameTilts)
+    lastFrame = min(firstFrame + frPerWindow - 1, length(perFrameTilts));
+    tiltRange(firstFrame) = max(prunedTilts(firstFrame:lastFrame)) -  ...
+			    min(prunedTilts(firstFrame:lastFrame));
   end
 end
-
-
 
 
 %% findThresholdForPercentile
@@ -48,4 +47,91 @@ end
 
 %% to test
 %%  [s1, s2, s3, s4, s5] = allTiltFeatures([9 8 7 1 2 3 9 6 6], [1 0 0 1 1 1 0 1 0], 10)
-%%  [s1, s2, s3, s4, s5] = allTiltFeatures([-.1 +.1 -.2 -.3 -.1 -.7 -.3 -3 -.2], [1 1 1 1 1 1 1  0 1], 10)
+%%  [st, tr, tf, tm, tn] = allTiltFeatures([-.1 +.1 -.2 -.3 -.1 -.8 -.3 -3 -.2 -.1 -.1], [1 1 1 1 1 1 1  0 1 1 1], 10)
+
+%% Midlevel Tilt-Based Features: Design Notes
+%% 
+%% Jan 22, 2023, Nigel Ward
+%% 
+%% The aim of this research memo is to document thoughts on new midlevel
+%% features based on spectral tilt.
+%% 
+%% By plotting the raw spectral tilt values, frame-by-frame, it's clear
+%% that they are exceedingly noisy.  We expect spectral tilt to depend
+%% primarily on the phoneme: most positive for fricatives, most negative
+%% for vowels, but examination indicates that even within one phoneme the
+%% tilt can vary greatly.
+%% 
+%% So we need to aggregate it into something more stable.  In general,
+%% perception-related prosodic features seem to be at least 50-100 ms
+%% wide, so anything at narrower spans is not needed.
+%% 
+%% For most prosodic features the mean seems the most obvious candidate
+%% for aggregation; at least for pitch and intensity.  However previous
+%% research suggests that range may be more informative, at least for
+%% predicting hyperarticulation.
+%% 
+%% The distribution of spectral tilt values appears to never have been
+%% discussed in the literature.  In our data, it is usually zero at
+%% moments of silence, generally slightly negative, occasionally very
+%% negative, and very occasionally positive.  Is it appropriate to take
+%% the mean?  
+%% 
+%% Listening to a few samples of timepoints in each of the three
+%% categories, suggests no. Both positive and very negative tilts often
+%% occur in utterances that seem emphatic, perhaps in different ways.
+%% Additionally the points with very shallow negative slope seem
+%% generally conversationally unimportant, while points with moderate,
+%% average-ish negative slope seem like normal conversation.  All of
+%% these, especially the last, are based on very few observations.  But
+%% it already seems clear that by taking the average we'd lose 
+%% information, at least for significantly positive points. 
+%% 
+%% It also looks like the distributions vary with the speaker.  We don't
+%% know if this is because speakers differ in typical tilt ranges, and
+%% only variation from their typical matters, or whether the meanings of
+%% different levels of tilt have absolute, cross-speaker meanings. For
+%% now we'll assume the former, and normalize everything to the speaker's
+%% distribution as computed over each track processed, using the
+%% percentile trick, as done for pitch.
+%% 
+%% Anyway, given that average would be a mistake, what to do?
+%% 
+%% 1) First we can compute the overall Tilt Range, as a measure likely to
+%% correlate with emphasis and articulation.  And in addition we'd like
+%% to compute the propensity of tilt to be in various ranges.
+%% Positive-tilt points are so few, perhaps 1 per minute, that an
+%% explicit feature for them wouldn't do us much good. Their contribution
+%% instead will just be in their effects on the tilt range.  This range
+%% feature could be expressed in the difference in percentiles between
+%% the highest point in the window and the lowest one.
+%% 
+%% 2) Second, we want to catch those Very Negative points.  It seems
+%% likely that more negative slopes are more extreme (strongly
+%% emphasized, perhaps) and if so, our Very Negative feature should count
+%% those more strongly.  Let's say that every point below the 20th
+%% percentile pitch counts to this according to how far it is below that
+%% threshold.  That is, a 19th percentile tilt point would contribute 1
+%% to this feature, and a 2nd percentile point would contribute 18.
+%% Should we subsequently z-normalize this for each track? Probably not.
+%% 
+%% 3) Third, we need a Nearly Flat feature.  It's possible this will
+%% correlate hugely with low intensity, but if so, we'll find that out.
+%% This might might be a fixed range, but, prefering to keep things in
+%% percentiles, this could be from the 75th to the 98th percentile,
+%% evenly weighted.  The 98th percentile would avoid including positive
+%% pitch points, since they are rare, and probably also many points
+%% infinitesimally close to zero.
+%% 
+%% 4) Fourth, we need a Middling Negative feature.  For tidiness, this
+%% could cover all remaining points, that is, those from the 20th to 75th
+%% percentile.  
+%% 
+%% In general, a bank of trapezoidal filters is prefereable.  Here the
+%% Very Negative is triangular, and the Nearly Flat and Middling Negative
+%% are rectangular for the sake of implementation simplicity and
+%% efficiency.   This could be revisited later. 
+%% 
+%% Note that these 4 features will all have only ever 0-or-positive
+%% values, and be far from normally distributed.
+
--- src/meansOverNonzeros.m
+++ src/meansOverNonzeros.m
@@ -5,25 +5,24 @@
 
 %% Nigel Ward, January 2023
 
-function means = meansOverNonzeros(featVec, isValid, frPerWindow)
-  integralImage = [0 cumsum(featVec)]
-  integralIsValidCount = [0 cumsum(isValid)]
+function means = meansOverNonzeros(featVec, isValid, frPerWindow, filler)
+  integralImage = [0 cumsum(featVec)];
+  integralIsValidCount = [0 cumsum(isValid)];
   windowSums = integralImage(1+frPerWindow:end) - ...
     	       integralImage(1:end-frPerWindow)
   windowDenominators = integralIsValidCount(1+frPerWindow:end) - ...
 		       integralIsValidCount(1:end-frPerWindow)
   means = windowSums ./ windowDenominators;
-
-  %% pad front and back; also replace NaNs
+  means(isnan(means)) = filler;
 
   globalMeanOfValids = mean(featVec(isValid==true));
   if frPerWindow > 1   % the normal case 
-    headFramesToPad = floor(frPerWindow / 2) - 1  ;
+    headFramesToPad = floor(frPerWindow / 2) - 1;
     tailFramesToPad = ceil(frPerWindow / 2);
     headPadding = globalMeanOfValids * ones(1,headFramesToPad);
     tailPadding = globalMeanOfValids * ones(1,tailFramesToPad);
     means = horzcat(headPadding, means, tailPadding);
-  means(isnan(means)) = globalMeanOfValids;
+  means(isnan(means)) = globalMeanOfValids
 end 
 
 
